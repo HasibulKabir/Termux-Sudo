91637305c0faa751416f84156d2723b5295f49fb
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-10-12 22:39:17 +0600

    Create sudo.sh

---
+++ sudo.sh
@@ -0,0 +1,17 @@
+cd $HOME
+
+apt install git
+
+git clone https://gitlab.com/st42/termux-sudo 
+
+cd termux-sudo
+
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+
+cd $HOME
+
+rm -rf termux-sudo
+
+echo "Successfully installed sudo"
